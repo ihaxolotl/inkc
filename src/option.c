8618828bece5d6fd72e0e38946e03b0e08d673fd
Jack Carter <jcarter777@protonmail.com>
2024-12-28 22:52:06 +0000

    replaced GNU getopt

---
+++ src/option.c
@@ -0,0 +1,79 @@
+#include <string.h>
+
+#include "option.h"
+
+// internal globals
+static const struct option *_g_option_opts;
+static char *_g_arg_ptr;
+static char **_g_option_argv;
+
+// exposed global for OPTION_OPERAND option
+char *option_operand;
+// exposed global for OPTION_UNKNOWN option
+char *option_unknown_opt;
+
+//
+// option_setopts - set internal globals to point to opts and argv
+//
+void option_setopts(const struct option *opts, char **argv) {
+        _g_option_argv = argv;
+        _g_option_opts = opts;
+}
+
+//
+// option_nextopt - get the .id field from the next option in the opts array
+//
+int option_nextopt(void) {
+        if (*_g_option_argv != NULL) {
+                ++_g_option_argv;
+        } else {
+                return 0;
+        }
+
+        char **optstr_p = _g_option_argv;
+        if (*optstr_p == NULL) {
+                return 0;
+        }
+
+        const struct option *p = _g_option_opts;
+        while (p->id != 0) {
+                if (!strcmp(*optstr_p, p->name)) {
+                        if (p->has_args) {
+                                _g_arg_ptr = optstr_p[1];
+                                ++_g_option_argv;
+                        }
+
+                        return p->id;
+                } else if (**optstr_p != '-') {
+                        option_operand = *optstr_p;
+                        return OPTION_OPERAND;
+                }
+
+                ++p;
+        }
+
+        option_unknown_opt = *optstr_p;
+        return OPTION_UNKNOWN;
+}
+
+//
+// option_nextarg - get the next argument from the last option with arguments
+//
+char *option_nextarg(void) {
+        char *arg = _g_arg_ptr;
+        char *arg_p = arg;
+
+        while (*arg_p != '\0' && *arg_p != ',') {
+                ++arg_p;
+        }
+
+        if (*arg_p == '\0') {
+                _g_arg_ptr = arg_p;
+                return arg;
+        }
+
+        _g_arg_ptr = arg_p + 1;
+        *arg_p = '\0';
+
+        return arg;
+}
